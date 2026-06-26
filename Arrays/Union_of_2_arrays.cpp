ebcc18aff80fa76099f2b7ec5a1c8a69d5fa070f
Jaskaran Singh <jaskaran.singh2024@vitstudent.ac.in>
2026-06-26 22:36:03 +0530

    Union of two arrays

---
+++ Arrays/Union_of_2_arrays.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void union_of_2_arrays(vector <int> array1 , vector <int> array2 ,int n , int m ,vector<int> &array3)
+{
+    int i = 0;
+    int j = 0;
+
+    while(i< n && j < m )
+    {
+        if (array1[i]<array2[j])
+        {
+            if(array3.empty () || array3.back() != array1[i] )
+                {
+                    array3.push_back(array1[i]);
+                }
+            i++;
+        }
+        else if(array1[i] > array2[j])
+        {
+            if(array3.empty () || array3.back() != array2[j] )
+                {
+                    array3.push_back(array2[j]);
+                }
+            j++;
+        }
+        else if(array1[i] == array2[j])
+        {
+            array3.push_back(array1[i]);
+            i++;
+            j++;
+        }
+    }
+    while(i< n)
+    {
+        if(array3.empty () || array3.back() != array1[i] )
+                {
+                    array3.push_back(array1[i]);
+                }
+        i++;
+    }
+
+    while( j < m)
+    {
+        if(array3.empty () || array3.back() != array2[j] )
+                {
+                    array3.push_back(array2[j]);
+                }
+        j++;
+    }
+}
+
+int main()
+{
+    int n;
+    int m;
+    cin >> n;
+    cin >> m;
+    vector <int> array1;
+    vector <int> array2;
+    vector <int> array3;
+
+    for(int i = 0 ; i < n ;i++)
+    {
+        int x;
+        cin >> x;
+        array1.push_back(x);
+    }
+
+    for(int i = 0 ; i < m ; i++)
+    {
+        int x;
+        cin >> x;
+        array2.push_back(x);
+    }
+
+    union_of_2_arrays(array1,array2,n,m,array3);
+
+    for(int i = 0 ; i < array3.size() ; i++)
+    {
+        cout << array3[i] << " ";
+    }
+}
+
+
